8fde45a460c28e7d3d0854ce0f7055d518578c81
najmi9 <imadnajmi9@gmail.com>
2022-08-23 22:50:21 +0100

    Change the query parameter lastEvent id

--- assets/js/mercure.js
+++ assets/js/mercure.js
@@ -1,13 +1,16 @@
 import { EventSourcePolyfill } from 'event-source-polyfill';
 
+const LAST_EVENT_ID_KEY = 'lastEventId';
+
 export default function mercure() {
     const hubUrl = new URL(document.body.dataset.url);
 
     hubUrl.searchParams.append('topic', '/users/1');
 
-    const lastEventId = localStorage.getItem('lastEventId');
+    const lastEventId = localStorage.getItem(LAST_EVENT_ID_KEY);
+
     if (null !== lastEventId){
-        hubUrl.searchParams.append('Last-Event-ID', lastEventId);
+        hubUrl.searchParams.append('lastEventID', lastEventId);
     }
 
     const subscriptionToken = document.body.dataset.token;
@@ -16,7 +19,6 @@ export default function mercure() {
         headers: {
             Authorization: 'Bearer ' + subscriptionToken,
         },
-        lastEventIdQueryParameterName: 'Last-Event-Id',
     };
 
     const eventSource = new EventSourcePolyfill(hubUrl, headers);
@@ -25,7 +27,7 @@ export default function mercure() {
      * @param {MessageEvent} e
      */
     eventSource.onmessage = (e) => {
-        localStorage.setItem('lastEventId', e.lastEventId);
+        localStorage.setItem(LAST_EVENT_ID_KEY, e.lastEventId);
         const data = JSON.parse(e.data);
         console.log({data});
     };
